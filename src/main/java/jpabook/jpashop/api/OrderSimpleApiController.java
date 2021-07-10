a1c438fe4f26840d15d649d866530b9306bf8fee
Gunwoo Park <prayzz12@gmail.com>
2021-07-10 22:14:01 +0900

    jpa shop - api 컨트롤러 실습

---
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop.api;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * xToOne (ManyToOne, OneToOne)
+ * Order
+ * Order -> Member
+ * Order -> Delivery
+ */
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/sample-orders")
+    public List<Order> ordersV1(){
+        List<Order> all = orderRepository.findAll(new OrderSearch());
+        return all;
+    }
+}
